2defecc97ae592507da0068ccf40e074f7c37578
Shodan <mycrash_forever@hotmail.com>
2022-08-17 23:41:43 -0500

    comilla

--- pages/pruebalol.tsx
+++ pages/pruebalol.tsx
@@ -97,7 +97,7 @@ const IndexPage: React.FC<IndexPageProps> = ({ products }) => {
   return (
     <Box p={4} align="center">
     <Stack>
-    <Tabs isFitted align="center" variant='enclosed >
+    <Tabs isFitted align="center" variant='enclosed'>
       
       <TabList boxSize="-moz-min-content" >
         <Tab _selected={{ color: 'white', bg: 'red.400' }}>Ofertas</Tab>
