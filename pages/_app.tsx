723edfd4aa22e4bbb68d0a5faa8561e078e0d241
Shodan <mycrash_forever@hotmail.com>
2022-08-25 05:39:58 +0200

    logo f

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           padding={4}
         >
           <VStack mb={6}>
-            <Image borderRadius="50%" src="https://www.facebook.com/photo/?fbid=7915004155207221&set=a.206854906022223" />
+            <Image borderRadius="50%" src="https://scontent.fjau2-1.fna.fbcdn.net/v/t39.30808-6/299606797_7915004071873896_4935666474124322977_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFPEQT_HqY20huYzzmD_Dznj0vIVAixSuWPS8hUCLFK5VcrnWwGwNaZlkipT3MNhsDyxiq2ZTwPtxdfFP8CaH0b&_nc_ohc=HPDb77sydG8AX-28Ums&_nc_ht=scontent.fjau2-1.fna&oh=00_AT_vpWyCHOgaEtxlomDzBNTONV2AN6Nlkjc16idXkqFMCQ&oe=630C3627" />
             
           </VStack>
           <Divider my={6} />
